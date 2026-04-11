246ef04e9d24987eb7dc98ee1e73f3f1903fd5d7
SubbotinValery <subbotin.valery.evgenyevich@gmail.com>
2026-04-11 21:00:08 +0300

    style: set 2 spaces indentation

--- eslint.config.js
+++ eslint.config.js
@@ -7,57 +7,57 @@ import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-    { ignores: ['dist', 'node_modules', 'build', '*.config.js', '*.config.ts'] },
+  { ignores: ['dist', 'node_modules', 'build', '*.config.js', '*.config.ts'] },
 
-    {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
-        files: ['src/**/*.{ts,tsx}'],
-        ignores: ['**/*.config.js', '**/*.config.ts', 'eslint.config.js'],
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: {
-                project: './tsconfig.app.json',
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-            prettier: prettierPlugin,
-        },
-        settings: {
-            'import/resolver': {
-                typescript: {
-                    project: './tsconfig.app.json',
-                },
-            },
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            'prettier/prettier': 'warn',
-            '@typescript-eslint/no-unused-vars': [
-                'warn',
-                {
-                    argsIgnorePattern: '^_',
-                    varsIgnorePattern: '^_',
-                },
-            ],
-            'no-console': ['warn', { allow: ['warn', 'error'] }],
-        },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['**/*.config.js', '**/*.config.ts', 'eslint.config.js'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.app.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.app.json',
+        },
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
 
-    {
-        files: ['**/*.config.js', '**/*.config.ts', 'eslint.config.js'],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.browser,
-            },
-        },
-        rules: {
-            '@typescript-eslint/no-var-requires': 'off',
-            'no-console': 'off',
-        },
-    }
+  {
+    files: ['**/*.config.js', '**/*.config.ts', 'eslint.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-console': 'off',
+    },
+  }
 );

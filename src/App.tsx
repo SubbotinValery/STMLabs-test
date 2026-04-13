f8af3d6b91de2a8fce4e45dea5f18748a5683e2e
SubbotinValery <subbotin.valery.evgenyevich@gmail.com>
2026-04-14 01:31:03 +0300

    test: verify data fetching in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import {} from 'react';
+import { useUsers } from './features/users/hooks/useUsers';
 
 function App() {
+  const { users } = useUsers();
+
   return (
     <>
       <div>STMLABS-test</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Изображение</th>
+            <th>Метоположение</th>
+            <th>Email</th>
+            <th>Телефон</th>
+            <th>Дата регистрации</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>
+                <img src={user.avatarThumb} alt={user.name} />
+              </td>
+              <td>{user.location}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.registeredDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
